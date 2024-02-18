4b69b960be4a19b9a611decf3d231e44c9cefe45
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2024-02-18 16:14:45 -0300

    install react-router and delete default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App () {
   return (
-    <h1>Hola</h1>
+    <BrowserRouter>
+              <Routes>
+                <Route path='/' element={<h1>Hola mundo</h1>} />
+              </Routes>
+            </BrowserRouter>
   )
 }
 
