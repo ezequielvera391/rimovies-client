020cc1498b358abf5088db8681ffd917dc938b0f
ezequielvera391 <114097815+ezequielvera391@users.noreply.github.com>
2024-02-18 22:42:40 -0300

    feat: add config and paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@hooks': path.resolve(__dirname, 'src/hooks'),
+      '@mocks': path.resolve(__dirname, 'src/mocks'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@public': path.resolve(__dirname, 'public')
+    }
+  },
   plugins: [react()]
 })
